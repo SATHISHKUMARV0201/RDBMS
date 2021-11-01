cc9663948ed8207507f67833e614fa661a64332c
SATHISHKUMAR V <93432798+SATHISHKUMARV0201@users.noreply.github.com>
2021-11-01 16:49:12 +0530

    Create index.sql

---
+++ index.sql
@@ -0,0 +1,10 @@
+create database stud;
+use stud;
+create table student
+(studid int,
+studname varchar(25),
+mark1 int,
+mark2 int,
+mark3 int);
+insert into student value(5,'narayanan',88,79,56);
+select * from student;
